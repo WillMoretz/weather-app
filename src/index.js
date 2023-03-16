7bee3a2f2ad8c82360db5666cf889b37381c85a5
Will Moretz <whmoretz@gmail.com>
2023-03-16 12:37:43 -0400

    added temp toggling

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import searchForCurrentWeather from "./api";
 
+const tempUnits = (() => {
+  let currentUnit = "F";
+
+  function toggle() {
+    // eslint-disable-next-line no-unused-expressions
+    currentUnit === "F" ? (currentUnit = "C") : (currentUnit = "F");
+  }
+  function getUnit() {
+    return currentUnit;
+  }
+
+  return { toggle, getUnit };
+})();
+
 const display = (() => {
   const clouds = document.querySelector(".clouds");
   const location = document.querySelector(".location");
@@ -32,9 +46,11 @@ const display = (() => {
     clouds.textContent = weather.clouds;
     location.textContent = weather.search;
     date.textContent = new Date().toDateString();
-    temp.textContent = weather.temp;
+    temp.textContent = `${Math.round(weather.temp)} °${tempUnits.getUnit()}`;
     humidity.textContent = `Humidity: ${weather.humidity}%`;
-    feel.textContent = `Feels like: ${weather.feel}`;
+    feel.textContent = `Feels like: ${Math.round(
+      weather.feel
+    )} °${tempUnits.getUnit()}`;
     air.textContent = updateAQI(weather.air);
   }
 
@@ -43,7 +59,7 @@ const display = (() => {
 
 function search() {
   let location = document.querySelector("#location").value;
-  const units = "imperial";
+  const units = tempUnits.getUnit() === "F" ? "imperial" : "metric";
   // Clean up input a bit
   while (true) {
     if (location.charAt(0) === " ") location = location.slice(1);
@@ -58,5 +74,13 @@ function search() {
 
 document.addEventListener("submit", (e) => {
   e.preventDefault();
+  search();
+});
+
+document.querySelector(".temp-toggle").addEventListener("click", () => {
+  tempUnits.toggle();
+  document.querySelector(
+    ".active-temp"
+  ).textContent = `°${tempUnits.getUnit()}`;
   search();
 });
